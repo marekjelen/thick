e93831c00ed8c3384231735f949aa189d0f4a702
Marek Jelen <marek@jelen.biz>
2012-03-31 12:18:03 +0200

    Necessary Java imports and requirements

--- lib/thick.rb
+++ lib/thick.rb
@@ -5,9 +5,13 @@ require 'java'
 require File.expand_path('../jars/netty-3.4.0.Alpha2.jar', __FILE__)
 
 require 'thick/java'
+require 'thick/thick'
 require 'thick/version'
 require 'thick/buffer'
 require 'thick/async_response'
+require 'thick/loader'
 require 'thick/server_handler'
 require 'thick/pipeline_factory'
+require 'thick/controller'
 require 'thick/server'
+
