f979d83556946393f8d6b4f988af77ba2fb011ae
Marek Jelen <marek@jelen.biz>
2012-03-31 12:16:08 +0200

    Refactored, better information

--- lib/thick/server.rb
+++ lib/thick/server.rb
@@ -1,29 +1,23 @@
 module Thick
 
-  def self.start(application, options)
-    @server = Server.new(application)
-    @server.start(:address => options[:address], :port => options[:port])
-  end
-
   class Server
 
-    def initialize(application)
+    def initialize(options)
+      @options = options
       @factory = Thick::Java::NioServerSocketChannelFactory.new(Thick::Java::Executors.new_cached_thread_pool, Thick::Java::Executors.new_cached_thread_pool)
       @bootstrap = Thick::Java::ServerBootstrap.new(@factory)
-      @bootstrap.pipeline_factory = PipelineFactory.new(application)
+      @bootstrap.pipeline_factory = PipelineFactory.new(@options)
     end
 
-    def start(options = {})
-      @channel = @bootstrap.bind(Thick::Java::InetSocketAddress.new(options[:address], options[:port]))
-      puts 'Server started'
-      loop do
-        # ToDo: Is this hack needed?
-        sleep(100)
-      end
+    def start
+      @channel = @bootstrap.bind(Thick::Java::InetSocketAddress.new(@options[:address], @options[:port]))
+      puts "* Server started on #{@options[:address]}:#{@options[:port]}"
+      self
     end
 
     def stop
       @channel.close
+      self
     end
 
   end
