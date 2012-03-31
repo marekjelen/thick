822159b243dd2c74209637dc3bf85635e8db8ab9
Marek Jelen <marek@jelen.biz>
2012-03-31 12:00:11 +0200

    Extract server executable starter method

---
+++ lib/thick/thick.rb
@@ -0,0 +1,28 @@
+module Thick
+
+  def self.start(options)
+    @options = {
+        :address => '0.0.0.0',
+        :port => 9292,
+        :environment => 'development',
+        :reloader => false,
+        :controller => false,
+        :directory => Dir.getwd
+    }.merge(options)
+
+    puts "* Starting Thick: #{@options.inspect}"
+
+    @options[:application] = Loader.new(@options)
+    @server = Server.new(@options).start
+
+    if @options[:controller]
+      @controller = Controller.new(@options, @server)
+      @controller.start
+    end
+
+    loop do
+      sleep(100) # ToDo: Is this hack needed?
+    end
+  end
+
+end
